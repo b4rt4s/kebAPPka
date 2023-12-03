be5550b4e288e786b9fd40d4a01f98151e379e4a
KrzysztofiniOWO <91846431+KrzysztofiniOWO@users.noreply.github.com>
2023-12-03 22:23:20 +0100

    Added user model and registration

--- Users/models.py
+++ Users/models.py
@@ -1,3 +1,40 @@
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
-# Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email=None, password=None, **extra_fields):
+        if not username:
+            raise ValueError('The Username must be set')
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(username, email, password, **extra_fields)
+
+class CustomUser(AbstractUser):
+    first_name = None
+    last_name = None
+    is_restricted = models.BooleanField(default=False, null=True)
+    phone_number = models.CharField(max_length=20)
+    location_consent = models.BooleanField(default=False)
+    marketing_consent = models.BooleanField(default=False)
+
+    AUTHENTICATION_TYPE_CHOICES = (
+        ('email', 'Email'),
+        ('phone', 'Phone'),
+    )
+
+    authentication_type = models.CharField(max_length=10, choices=AUTHENTICATION_TYPE_CHOICES, default='email')
+
+    objects = CustomUserManager()
